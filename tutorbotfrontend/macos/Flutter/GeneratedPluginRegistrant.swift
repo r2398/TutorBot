3de790c042476b8f34b2e1ed9899dc963a461771
henlo7z <msri874@aucklanduni.ac.nz>
2025-12-18 18:19:52 +0530

    minorUpdates

--- tutorbotfrontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tutorbotfrontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,7 @@ import file_selector_macos
 import flutter_tts
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
+import speech_to_text
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
